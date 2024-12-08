b35ec87f272d9c6a015498801174057f77e4eaf6
Shohruh <shoh0554770876@gmail.com>
2024-12-08 12:53:41 +0300

    Update iris.py

--- iris.py
+++ iris.py
@@ -11,15 +11,6 @@ columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'specie
 iris = pd.read_csv(url, header=None, names=columns)
 
 
-
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.datasets import make_blobs
-import plotly.express as px
-
 # Генерация случайных данных
 X, y = make_blobs(n_samples=1000, centers=3, n_features=2, random_state=42)
 df = pd.DataFrame(X, columns=['x', 'y'])
